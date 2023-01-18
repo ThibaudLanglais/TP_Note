a429d30c9d246aa05fac81c0a84133e66f17221c
Brigurde <48880155+Brigurde@users.noreply.github.com>
2023-01-18 15:34:23 +0100

    init

---
+++ src/test/java/com/ensim/tpnote/tpnote/TpnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ensim.tpnote.tpnote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
